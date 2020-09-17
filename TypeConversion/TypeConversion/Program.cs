4d85a0522ecb854355a743e67f24462be7c069e0
wuh0007 <wuh007@udayton.edu>
2020-09-17 16:17:18 -0400

    first commit

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            int m = 1;
+            byte n = (byte)m;
+            Console.WriteLine(n);
+
+            var num = "1234";
+            int k = Convert.ToInt32(num);
+            Console.WriteLine(k);
+
+
+            try
+            {
+                var number = "1234";
+                int j = Convert.ToByte(number);
+                Console.WriteLine(j);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
---
+++ VariablesConstants/VariablesConstants/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VariablesConstants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = 2;
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mosh";
+            bool isWorking = false;
+
+            // variables
+            Console.WriteLine(num);
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            // format 
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            // const
+            const float Pi = 3.14f; // cannot be changed
+
+
+        }
+    }
+}
