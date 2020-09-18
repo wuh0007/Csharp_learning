f410c5a4869a848c5e6cf80203d63779190b681e
wuh0007 <wuh007@udayton.edu>
2020-09-18 15:40:47 -0400

    Sep 18th commit

--- ArraysStrings/ArraysStrings/Program.cs
+++ ArraysStrings/ArraysStrings/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ArraysStrings
 {
@@ -10,6 +6,35 @@ namespace ArraysStrings
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            string firstName = "Hongyu";
+            String lastName = "Wu";
+            var fullName = firstName + " " + lastName;
+            //var myFullName = string.Format("My name is {0) {1}", firstName, lastName);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"hi guys
+                        Look into the following paths
+                        c:\folder1\folder2
+                        c:\folder3\folder4";
+            Console.WriteLine(text);
         }
     }
 }
