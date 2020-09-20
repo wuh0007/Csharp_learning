f42f9a2d27948c73bcb0c1eec6d9a847e7fbc683
wuh0007 <wuh007@udayton.edu>
2020-09-20 19:13:19 -0400

    Sep 20th 2nd commit

---
+++ SummarizingTextProb/SummarizingTextProb/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizingTextProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is a long sentence .... LOL LLLLOL OLLLOOL LLoL";
+            var summary = StringUtility.SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+    }
+}
--- WorkWithText/WorkWithText/Program.cs
+++ WorkWithText/WorkWithText/Program.cs
@@ -10,6 +10,31 @@ namespace WorkWithText
     {
         static void Main(string[] args)
         {
+            var fullName = "Hongyu Wu";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.ToUpper());
+
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("FirstName: " + firstName);
+            Console.WriteLine("LastName: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("FirstName: " + names[0]);
+            Console.WriteLine("LastName: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Hongyu", "Genius"));
+
+            if (String.IsNullOrWhiteSpace(null))
+                Console.WriteLine("Invalid");
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C")); 
         }
     }
 }
