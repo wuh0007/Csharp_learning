a99ce3e24273bfb76158d20d7aa79a90ecf73120
wuh0007 <wuh007@udayton.edu>
2020-09-20 22:53:59 -0400

    Sep 20th 2nd commit

---
+++ ProceduralProgramming/ProceduralProgramming/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProceduralProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+
+            Console.WriteLine("Reversed name: " + reversed);
+
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - 1] = name[i - 1];
+            }
+
+            return new string(array);
+        }
+
+    }
+}
---
+++ WorkWithFiles/WorkWithFiles/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace WorkWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\haha.jpg";
+
+            File.Copy(@"c:\temp\me.jpg", @"d:\temp\me.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //some codes
+            }
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //some codes
+            }
+        }
+    }
+}
